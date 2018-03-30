57af10c6ae641a07a0eb241b8a92a8c6374d3013
saqot <saqott@gmail.com>
2018-03-30 23:03:55 +0700

    start

---
+++ engine/data/dbconfig.php
@@ -0,0 +1,21 @@
+<?PHP
+
+define ("DBHOST", "localhost");
+
+define ("DBNAME", "dle_121");
+
+define ("DBUSER", "root");
+
+define ("DBPASS", "");
+
+define ("PREFIX", "dle");
+
+define ("USERPREFIX", "dle");
+
+define ("COLLATE", "utf8");
+
+define('SECURE_AUTH_KEY', ' [K!-d/h3L3S=]h3?Jn+D}Bb2H4]8HC}8 PZb6Ir/[g;!|;/@2lb#0#sXiLl-aM');
+
+$db = new db;
+
+?>
